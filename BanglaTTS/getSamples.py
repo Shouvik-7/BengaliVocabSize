bfa5a9a5801a45095e2e9abf82c27748af630747
Shouvik <64649488+Shouvik-7@users.noreply.github.com>
2023-09-04 17:27:15 -0600

    Add files via upload

---
+++ BanglaTTS/getSamples.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import csv
+
+def getsample(filename):
+    df = pd.read_csv(filename,quoting=csv.QUOTE_NONE)
+    df = df[df['\t0'] != '"']['\t0'].apply(lambda x: x.split('\t')[1])
+    df = df.apply(lambda x: x.replace('"', ''))
+    df = df.apply(lambda x:x.strip(" "))
+    df.name = 'words'
+    print(len(df))
+    for n in range(10):
+        df.sample(20).to_csv(f'./banglatts/Samples/{n}.csv',index=False)
+
+if __name__ == '__main__':
+    getsample('D:/New folder/NLP_assignment/as01/BanglaTTS/words.csv')
